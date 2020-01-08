55bcaac9673c1422f7f02ad934f7be0189681bc1
preciouStar <seongjin605@users.noreply.github.com>
2020-01-09 00:08:50 +0900

    defer 선언 함수 동작 확인

--- 07_function/main.go
+++ 07_function/main.go
@@ -37,9 +37,33 @@ func callMultiValues() (int, int) {
 	return 5, 6
 }
 
+func first() {
+	fmt.Println("1st")
+}
+func second() {
+	fmt.Println("2nd")
+}
+func third() {
+	fmt.Println("3rd")
+}
 func main() {
 	callInputValue(10)
 	callFive()
 	x, y := callMultiValues()
 	fmt.Println(x, y)
+
+	// 함수안에 함수를 만듬(클로저)
+	add := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(add(1, 1))
+
+	// defer는 어떤 식으로든 자원을 해제해야 할 때 자주 사용된다.
+	// 이것은 해당 함수가 실행을 완료했을 때 실행을 위해 호출 스케줄을 잡는다.
+	// 예를 들어, 파일을 열 때 나중에 해당 파일을 반드시 닫아야 한다.
+	// second가 항상 맨마지막에 실행이되므로, 자원해제 함수를 defer로 선언하면 됨.
+	defer second()
+	first()
+	third()
+
 }
